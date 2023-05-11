231ef9a1583574f05846d63dc8f806211fb96a52
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 08:59:11 +0530

    returning value added to array

--- index.js
+++ index.js
@@ -4,7 +4,20 @@
 // const arr = [3,5,8]
 // arr.map(hi)
 
-const students = ["Harry","Ron","Hermione","Draco"]
-students.map((stude)=>{
+const students = ["Harry", "Ron", "Hermione", "Draco"]
+students.map((stude) => {
     console.log(`hello ${stude}!`);
 })
+
+// returning value added to array 
+const nums = [3, 5, 8, 9, 15, 18]
+ const result = nums.map((num) => {
+    if (num % 2 == 0) {
+        return `${num} is even`
+    }
+    else{
+        return `${num} is odd`
+        
+    }
+})
+console.log(result)

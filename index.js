5b13852a0043489ba59e966d05af662f875970c1
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 08:36:54 +0530

    sqaure calculate by map func

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function hi (p){
-console.log(`Hiee ${p}`);
+// function hi (p){
+// console.log(`Hiee ${p}`);
+// }
+// const arr = [3,5,8]
+// arr.map(hi)
+
+function square(num){
+    console.log(num*num);
+
 }
-const arr = [3,5,8]
-arr.map(hi)
+ const arr = [ 1, 2, 3 ,4 ,5]
+ arr.map(square)

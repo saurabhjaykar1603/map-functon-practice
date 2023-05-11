58f11836dc3a6aac0d85b18656c0829b0f3c5bc9
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 09:25:53 +0530

    after creating component change value/name using props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Student from './Student'
 
 function App() {
   return (
     <div>
+      <h1>React app</h1>
+      <Student name='Anand sir'/>
+      <Student name='Suraj sir'/>
+    <Student name='Vaibhavi Mam'/>
       
     </div>
   )
---
+++ src/Student.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Student.css'
+
+function student(prop) {
+  return (
+    <div className='student-container'>
+    hello {prop.name}
+    </div>
+  )
+}
+
+export default student
